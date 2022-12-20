ec9f7dd22da054fcf53342d5001ed3ce67284e40
Steffen Schmidt <steffen.schmidt.ss1@roche.com>
2022-12-20 20:12:11 +0100

    fix pipeline secrets

--- 03_02-03_remotestate/main.tf
+++ 03_02-03_remotestate/main.tf
@@ -15,9 +15,9 @@ terraform {
 # //////////////////////////////
 # VARIABLES
 # //////////////////////////////
-variable "aws_access_key" {}
+#variable "aws_access_key" {}
 
-variable "aws_secret_key" {}
+#variable "aws_secret_key" {}
 
 variable "region" {
   default = "eu-central-1"
@@ -35,8 +35,8 @@ variable "subnet1_cidr" {
 # PROVIDERS
 # //////////////////////////////
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  #access_key = var.aws_access_key
+  #secret_key = var.aws_secret_key
   region     = var.region
 }
 
